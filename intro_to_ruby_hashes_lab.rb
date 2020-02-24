a4bdd97db7a262c0b346ce7166ca5684556b64cb
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-24 20:07:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,14 +15,12 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-my_hash_creator={key:"value"}
-return my_hash_creator
+
 end
 
 def read_from_hash(hash, key)
   hash={key}
-  read_from_hash(hash)
-  
+  read_from_hash={key:"value"}
 
 end
 
